24f331c43769f788cb900c8c2d291c9c89b7e955
Sergey Zhiltsov <zhiltsov.s.i@gmail.com>
2023-03-11 16:19:03 +0100

    Add argument to extend include and exclude patterns

--- build_report.py
+++ build_report.py
@@ -1,21 +1,27 @@
 # -*- coding: utf-8 -*-
 import os
 import re
+import argparse
+import contextlib
+
 from collections import namedtuple
 from functools import wraps
-
 from odf.text import LineBreak, H, P, Span, S
 from odf.opendocument import OpenDocumentText
 from odf.style import Style, TextProperties
-import contextlib
+
+parser = argparse.ArgumentParser(description="Reduce video size")
+parser.add_argument('-i', '--include', action='append', help="Add pattern to include", default=[])
+parser.add_argument('-e', '--exclude', action='append', help="Add pattern to exclude", default=[])
+args = parser.parse_args()
 
 
 def compile_regexes(*patterns):
     return tuple([re.compile(pattern, re.IGNORECASE) for pattern in patterns])
 
 
-EXCLUDE_PATTERNS = compile_regexes(r".*node_modules", r".*site-packages", r".*\.git", r".*\.idea")
-INCLUDE_EXTENSIONS = ("java", "[tj]sx", "py", "md", "txt", "json", "ya?ml", "sh")
+EXCLUDE_PATTERNS = compile_regexes(r".*node_modules", r".*site-packages", r".*\.git", r".*\.idea", r".*\.log$")
+INCLUDE_EXTENSIONS = ("java", "[tj]sx", "py", "md", "txt", "json", "ya?ml", "sh", "s?css", "conf", "html", "ini")
 INCLUDE_PATTERNS = compile_regexes(
     *(rf".*\.{ext}" for ext in INCLUDE_EXTENSIONS),
     r".*\.gitignore",
@@ -39,20 +45,22 @@ def any_matches(regexes, filename):
     return any((regex.match(filename) for regex in regexes))
 
 
-def include(filepath):
-    return not any_matches(EXCLUDE_PATTERNS, filepath) and any_matches(INCLUDE_PATTERNS, filepath)
+def include(filepath, include_patterns, exclude_patterns):
+    return (not any_matches(exclude_patterns, filepath)) and any_matches(include_patterns, filepath)
 
 
 @wrap_result(tuple)
-def get_files(base_path):
+def get_files(base_path, custom_include, custom_exclude):
+    full_include = INCLUDE_PATTERNS + compile_regexes(*(fr"{pattern}" for pattern in custom_include))
+    full_exclude = EXCLUDE_PATTERNS + compile_regexes(*(fr"{pattern}" for pattern in custom_exclude))
     for root, dirs, files in os.walk(base_path, followlinks=True):
         for filename in files:
             filepath = os.path.join(root, filename)
-            if include(filepath):
+            if include(filepath, full_include, full_exclude):
                 yield filepath
 
 
-Styles = namedtuple("Styles", ["file_title", "code"])
+StyleSheets = namedtuple("StyleSheets", ["file_title", "code"])
 
 
 @contextlib.contextmanager
@@ -68,13 +76,13 @@ def odt_doc(filename):
     code.addElement(TextProperties(attributes={'fontsize': "12pt", 'fontfamily': "Courier New"}))
     doc.styles.addElement(code)
 
-    yield doc, Styles(file_title, code)
+    yield doc, StyleSheets(file_title, code)
     doc.save(filename)
 
 
 def main():
     base_path = os.getcwd()
-    files = sorted(get_files(base_path))
+    files = sorted(get_files(base_path, args.include, args.exclude))
     count = len(files)
     print(f"{count} files found")
     with odt_doc(f"Report_{os.path.split(base_path)[-1]}.odt") as (doc, styles):
