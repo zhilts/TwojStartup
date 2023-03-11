e43441bfed4f6c7059a05a35c64950b2603066e5
Sergey Zhiltsov <zhiltsov.s.i@gmail.com>
2023-03-11 13:52:50 +0100

    Fix running from another directory

---
+++ build_report.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+$SCRIPT_DIR/init.sh
+. ${SCRIPT_DIR}/venv/bin/activate
+python $SCRIPT_DIR/build_report.py $@
--- init.sh
+++ init.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env sh
+set -x
 
-VENV="./venv"
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+VENV="${SCRIPT_DIR}/venv"
 if [ ! -d "$VENV" ]; then
   python3 -m venv $VENV
   echo "Installing config files in ${VENV}..."
 fi
 
-. $VENV/bin/activate
-pip install -r ./requirements.txt
+. ${VENV}/bin/activate
+pip install -r ${SCRIPT_DIR}/requirements.txt
