d256b532e9edbd8f616c487e0ec7c5714b43a45c
Sergey Zhiltsov <zhiltsov.s.i@gmail.com>
2023-03-11 16:18:38 +0100

    Fix run

--- build_report.sh
+++ build_report.sh
@@ -3,4 +3,4 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 $SCRIPT_DIR/init.sh
 . ${SCRIPT_DIR}/venv/bin/activate
-python $SCRIPT_DIR/build_report.py $@
+python $SCRIPT_DIR/build_report.py "$@"
